d11e4c436133adbaf078fe41970bec0754d82ddb
abang <839364887@qq.com>
2019-06-21 20:34:57 +0800

    把tabbar封装成一个组件

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import './App.css';
-import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
-function App() {
-  return (
-    <div className="App">
-      <Button>default</Button><WhiteSpace />
-    <Button disabled>default disabled</Button><WhiteSpace />
+import React,{ Fragment} from 'react';
+import './styles/App.css';
+import MyLayout from './components/MyLayout'
 
-    <Button type="primary">primary</Button><WhiteSpace />
-    <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
-
-    <Button type="warning">warning</Button><WhiteSpace />
-    <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
-
-    <Button loading>loading button</Button><WhiteSpace />
-    <Button icon="check-circle-o">with icon</Button><WhiteSpace />
-    <Button icon={<img src="https://gw.alipayobjects.com/zos/rmsportal/jBfVSpDwPbitsABtDDlB.svg" alt="" />}>with custom icon</Button><WhiteSpace />
-    <Button icon="check-circle-o" inline size="small" style={{ marginRight: '4px' }}>with icon and inline</Button>
-    <Button icon="check-circle-o" inline size="small">with icon and inline</Button>
-    </div>
-  );
+class TabBarExample extends React.Component {
+  render(){
+    return(
+      <Fragment>
+        <MyLayout />
+      </Fragment>
+    )
+  }
 }
 
-export default App;
+export default TabBarExample;
