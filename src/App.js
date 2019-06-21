5d0128c8b04495d7acf0b527934e4ca5e5ae700f
abang <839364887@qq.com>
2019-06-21 19:44:43 +0800

    使用ui组件按钮

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
 function App() {
   return (
     <div className="App">
-      123
+      <Button>default</Button><WhiteSpace />
+    <Button disabled>default disabled</Button><WhiteSpace />
+
+    <Button type="primary">primary</Button><WhiteSpace />
+    <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
+
+    <Button type="warning">warning</Button><WhiteSpace />
+    <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
+
+    <Button loading>loading button</Button><WhiteSpace />
+    <Button icon="check-circle-o">with icon</Button><WhiteSpace />
+    <Button icon={<img src="https://gw.alipayobjects.com/zos/rmsportal/jBfVSpDwPbitsABtDDlB.svg" alt="" />}>with custom icon</Button><WhiteSpace />
+    <Button icon="check-circle-o" inline size="small" style={{ marginRight: '4px' }}>with icon and inline</Button>
+    <Button icon="check-circle-o" inline size="small">with icon and inline</Button>
     </div>
   );
 }
